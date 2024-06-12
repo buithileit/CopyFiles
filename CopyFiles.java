3ae7911d639317517b43efd6e29e62a89c4b898c
buithileit <buithileit@gmail.com>
2024-06-13 00:40:54 +0700

    Add files via upload

---
+++ CopyFiles.java
@@ -0,0 +1,16 @@
+package copyfile;
+
+import java.nio.file.Files;
+import java.util.List;
+
+public class CopyFiles {
+	public static void main(String[] args) {
+		List<String> wLines = Files.readAllLines("ListFilesCopy.txt");
+		
+		wLines.forEach( e ->{
+			
+		});
+	}
+	
+	
+}
